e42ed1e17e533b659c7e9f094c93b0765597759d
KAUAN LOPES PEREIRA <25132925@dsn127m1241958.cfp127aluno.local>
2026-05-07 10:16:51 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
+import heroImg from './assets/hero.png'
+import './styles/index.css'
+
+function App() {
+  return (
+    <h1>Sempre Limpa</h1>
+  )
+}
+
+export default App
